d2047d6067797094bda133d8427908877e309c5b
eunkyoung99 <eunjee001@gmail.com>
2023-12-21 14:34:18 +0900

    인터페이스 추가 및 데이터 바인딩 적용

---
+++ app/src/main/java/com/kkyoungs/mvp/Contract.kt
@@ -0,0 +1,14 @@
+package com.kkyoungs.mvp
+
+import org.json.JSONObject
+
+interface Contract {
+    interface View{
+        fun showInfo(info:JSONObject)
+    }
+    interface Presenter{
+        fun initInfo()
+        fun setInfo(info :JSONObject)
+        fun saveInfo(info : JSONObject)
+    }
+}
